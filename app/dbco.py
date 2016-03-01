230970f233b43b65e82bc53bff3195b5967dab6c
Crishna Iyengar <ciyengar3@gmail.com>
2016-03-01 14:12:04 -0500

    db stuff

--- app/dbco.py
+++ app/dbco.py
@@ -1,7 +1,19 @@
 from pymongo import MongoClient
+from flask import Flask
 
 DBNAME = 'scribe'
 
-client = MongoClient('mongodb://localhost:27017/')
+client = MongoClient()
 
-db = client[DBNAME]
+db = client.joke_review
+
+joke_coll = db.jokes
+
+joke_1 = {"Joke": "Why did the chicken cross the road? Because he is stupid."}
+
+joke_json = Flask.jsonify(**joke_1)
+
+result = joke_coll.insert_one(joke_json)
+
+
+
--- app/views.py
+++ app/views.py
@@ -1,12 +1,45 @@
 from app import app
 from flask import render_template
 import jokes
+from pymongo import MongoClient
+import pymongo
+from flask import Flask
+import flask
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    jokesList = [jokes.Joke("Just changed my Facebook name to \'No one\' so when I see stupid posts "
-                        "I can click like and it will say \'No one\' likes this'.", 1),
+
+    print("hello")
+    client = MongoClient('mongodb://localhost:27017/')
+
+    db = client.joke_review
+
+    joke_coll = db.jokes
+
+
+    result_1 = joke_coll.insert_one({
+        "joke" : "Just changed my Facebook name to \'No one\' so when I see stupid posts I can click like and it will say \'No one\' likes this'."
+    })
+
+    result_2 = joke_coll.insert_one({
+        "joke" : "What's the difference between snowmen and snowladies? Snowballs"
+    })
+
+    result_3 = joke_coll.insert_one({
+        "joke" : "How do you make holy water? You boil the hell out of it"
+    })
+
+    cursor = db.jokes.find().sort([
+        ("joke", pymongo.ASCENDING),
+    ])
+
+    for joke in cursor:
+        print(joke)
+
+
+    jokesList = [jokes.Joke("Just changed my Facebook name to \'No one\' so when I see stupid posts I can click like and it will say \'No one\' likes this'.", 1),
              jokes.Joke("What's the difference between snowmen and snowladies? Snowballs",2),
              jokes.Joke("How do you make holy water? You boil the hell out of it",3)]
     return render_template('index.html',title ='Joke Review', jokes = jokesList)
