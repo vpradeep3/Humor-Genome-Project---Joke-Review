eee01130e150df74ee996c971239e6e046c71ea2
Crishna Iyengar <ciyengar3@gmail.com>
2016-03-01 14:37:26 -0500

    fixed db stuff

--- app/views.py
+++ app/views.py
@@ -34,12 +34,13 @@ def index():
     cursor = db.jokes.find().sort([
         ("joke", pymongo.ASCENDING),
     ])
-
+    jokesList = []
+    i = 0
     for joke in cursor:
         print(joke)
+        joke_string = joke["joke"]
+        joke_id = joke["_id"]
+        new_joke = jokes.Joke(joke_string, joke_id)
+        jokesList.append(new_joke)
 
-
-    jokesList = [jokes.Joke("Just changed my Facebook name to \'No one\' so when I see stupid posts I can click like and it will say \'No one\' likes this'.", 1),
-             jokes.Joke("What's the difference between snowmen and snowladies? Snowballs",2),
-             jokes.Joke("How do you make holy water? You boil the hell out of it",3)]
     return render_template('index.html',title ='Joke Review', jokes = jokesList)
