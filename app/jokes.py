b8fcb5d7ff1c6697d34ee693738cc6e759be7738
Erik Sampayo <ErikSampayo@gmail.com>
2016-03-07 21:54:56 -0500

    Added files via upload

--- app/jokes.py
+++ app/jokes.py
@@ -2,11 +2,12 @@ import random
 
 field_content = 'content'
 field_jokeId = 'jokeId'
+field_title = 'title'
 
 
 class Joke(object):
 
-    def __init__(self, content, jokeId=None):
+    def __init__(self, content, title, jokeId=None):
 
         self.content = str(content)
 
@@ -14,6 +15,8 @@ class Joke(object):
             jokeId = random.randint(54321, 16171617)
 
         self.jokeId = jokeId
+        
+        self.title = title;
 
 
     def to_json(self):
@@ -21,13 +24,16 @@ class Joke(object):
 
         myjoke[field_content] = self.content
         myjoke[field_jokeId] = self.jokeId
-
+        myjoke[field_title] = self.title
+        
         return myjoke
 
     @classmethod
-    def from_json(constructor, myuser):
+    def from_json(constructor, joke):
 
-        content = myuser[field_content]
-        jokeId = myuser[field_jokeId]
+        self.content = joke[field_content]
+        self.jokeId = joke[field_jokeId]
+        self.title = joke[field_title]
+        
 
         return constructor(content, jokeId)
